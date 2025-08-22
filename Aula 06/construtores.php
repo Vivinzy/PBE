badb0362ce1437f9123f24d82496de435a67543b
Vivinzy <vitorianunespera@gmail.com>
2025-08-22 16:47:30 -0300

    22.08.2025

--- Aula 06/construtores.php
+++ Aula 06/construtores.php
@@ -5,20 +5,28 @@ class Produtos {
     public $fornecedor;
     public $qtde_estoque;
 
-    public function produto_vendido($qtde_vendida): void {
-      
+    public function __construct($nome, $categoria, $fornecedor, $qtde_estoque){
+        $this->nome = $nome;
+        $this->categoria = $categoria;
+        $this->fornecedor = $fornecedor;
+        $this->qtde_estoque = $qtde_estoque;
+    }
+
+    public function produto_vendido($qtde_vendida): mixed{
+        $this->qtde_estoque -= $qtde_vendida;
+        return $this->qtde_estoque;
     }
 }
 
 
 // $bolacha1 = new Produtos("Nikito", "Doces", "Vitarella", 220);
-$bolacha1 = new Produtos();
+$bolacha1 = new produto.();
 $bolacha1->nome = "Nikito";
 $bolacha1->categoria = "Doces";
 $bolacha1->fornecedor = "Vitarella";
 $bolacha1->qtde_estoque = 220;
 
-$feijao1 = new Produtos();
+$feijao1 = new produto.();
 $feijao1->nome = "Oliron";
 $feijao1->categoria = "Mantimentos";
 $feijao1->fornecedor = "Reserva nobre";
